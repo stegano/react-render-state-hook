2a47b0cf76f620e57dc86e4c1964069d37d4dfa5
Yongwoo Jung <stegano@naver.com>
2024-01-05 01:02:46 +0900

    release v1.1.0

--- bin/hooks/use-render-state.interface.d.ts
+++ bin/hooks/use-render-state.interface.d.ts
@@ -1,20 +1,40 @@
 import { ReactNode } from "react";
 export declare enum DataHandlingStatus {
+    IDLE = "IDLE",
     IN_PROGRESS = "IN_PROGRESS",
-    SUCCEEDED = "SUCCEEDED",
-    FAILED = "FAILED"
+    COMPLETED = "COMPLETED",
+    FAILURE = "FAILURE"
 }
+/**
+ * `DataHandlerExecutor` is a function that handles data.
+ */
 export interface DataHandlerExecutor<Data extends any = any> {
     (previousData?: Data): Promise<Data> | Data;
 }
 export interface DataHandler<Data> {
-    (executor: DataHandlerExecutor<Data>, id?: string): Promise<Data>;
+    (executor: DataHandlerExecutor<Data>, executorId?: string): Promise<Data>;
 }
-export type RenderWhenDataHandlingSucceeded<Data> = ReactNode | ((data: Data, previousData?: Data) => ReactNode);
-export type RenderWhenDataHandlingInProgress<Data> = ReactNode | ((data?: Promise<Data>, previousData?: Data) => ReactNode);
-export type RenderWhenDataHandlingFailed<DataHandlingError extends Error | unknown = unknown, Data extends any = any> = ReactNode | ((error: DataHandlingError, previousData?: Data) => ReactNode);
+export interface DataResetHandler {
+    (): void;
+}
+/**
+ * `IdleRenderer` is rendered when the data handling status is `IDLE`.
+ */
+export type IdleRenderer<Data> = ReactNode | ((previousData?: Data) => ReactNode);
+/**
+ * `CompletedRenderer` is rendered when the data handling status is `COMPLETED`.
+ */
+export type CompletedRenderer<Data> = ReactNode | ((data: Data, previousData?: Data) => ReactNode);
+/**
+ * `InProgressRenderer` is rendered when the data handling status is `IN_PROGRESS`.
+ */
+export type InProgressRenderer<Data> = ReactNode | ((data?: Promise<Data>, previousData?: Data) => ReactNode);
+/**
+ * `FailureRenderer` is rendered when the data handling status is `FAILURE`.
+ */
+export type FailureRenderer<DataHandlingError extends Error | unknown = unknown, Data extends any = any> = ReactNode | ((error: DataHandlingError, previousData?: Data) => ReactNode);
 export interface Render<Data = any, DataHandlingError = any> {
-    (renderWhenDataHandlingSucceeded?: RenderWhenDataHandlingSucceeded<Data>, renderWhenDataHandlingInProgress?: RenderWhenDataHandlingInProgress<Data>, renderWhenDataHandlingFailed?: RenderWhenDataHandlingFailed<DataHandlingError, Data>): ReactNode;
+    (completedRenderer?: CompletedRenderer<Data>, idleRenderer?: IdleRenderer<Data>, inProgressRenderer?: InProgressRenderer<Data>, failureRenderer?: FailureRenderer<DataHandlingError, Data>): ReactNode;
 }
 export interface DataHandlingState<Data, DataHandlingError> {
     status: DataHandlingStatus;
@@ -26,6 +46,7 @@ export interface DataHandlingState<Data, DataHandlingError> {
 export type Renderer<Data, DataHandlingError> = [
     render: Render<Data, DataHandlingError>,
     handleData: DataHandler<Data>,
+    handleResetData: DataResetHandler,
     dataHandlingState: DataHandlingState<Data, DataHandlingError>
 ];
 export interface Options<Data extends any = any> {
