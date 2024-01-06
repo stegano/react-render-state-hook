ef9ef9cfc6369e72d02500ea6c95757448d22ee4
Yongwoo Jung <stegano@naver.com>
2024-01-06 22:41:34 +0900

    release v1.2.1

--- bin/providers/render-state.d.ts
+++ bin/providers/render-state.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="react" />
 import { Context, Props } from "./render-state.interface";
-export declare const defaultStore: import("../store/store.interface").Store<any, Record<string, any>>;
+import { DataHandlingState } from "../hooks/use-render-state.interface";
+export declare const defaultStore: import("../store/store.interface").Store<DataHandlingState<any, any>, Record<string, DataHandlingState<any, any>>>;
 export declare const RenderStateContext: import("react").Context<Context>;
 declare function RenderStateProvider({ children, dataHandlerExecutorInterceptorList, store, }: Props): import("react/jsx-runtime").JSX.Element;
 export default RenderStateProvider;
